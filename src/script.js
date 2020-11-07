070108e00b91cf2f20e0ec9e3a27ca019a0f0c87
Berni <bernatferrerm@gmail.com>
2020-11-07 23:52:20 +0100

    login page

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 particlesJS.load('particles-js', '../assets/particles.json', function () {
 
 
+    $("#nasa-logo").fadeIn(5000, () => $(".login-panel").fadeIn(2000));
+    $("#nasa-icon").fadeIn(2000);
 
 
 });
