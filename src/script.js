66609bb7ac265ece6ee558af47e08d9ddf17f23e
Berni <bernatferrerm@gmail.com>
2020-11-09 00:19:28 +0100

    login styles fixed

--- src/script.js
+++ src/script.js
@@ -9,7 +9,9 @@ window.onload = function () {
     particlesJS.load('particles-js', 'assets/particles.json', function () {
 
         loginTitle.fadeIn(5000);
-        nasaIcon.fadeIn(2000, () => loginPanel.fadeIn(3000))
+
+        nasaIcon.fadeIn(2000, () => loginPanel.fadeIn(3000));
+        pJSDom[0].pJS.particles.move.enable = false;
     });
 
 
