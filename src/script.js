a42b841db1758a185c141b4bd913fc7e67c6bcc3
Berni <bernatferrerm@gmail.com>
2020-11-09 12:15:27 +0100

    login and register

--- src/script.js
+++ src/script.js
@@ -3,20 +3,53 @@ window.onload = function () {
     var sectionLogin = $('.login');
     var loginTitle = $("#loginTitle");
     var nasaIcon = $("#nasa-icon");
-    var loginPanel = $(".login-panel")
+    var loginPanel = $(".login-panel");
 
+    var login = $(".option-login");
+    var register = $(".option-register");
+    var loginForm = $(".login-form");
+    var registerForm = $(".register-form");
 
     particlesJS.load('particles-js', 'assets/particles.json', function () {
 
         loginTitle.fadeIn(5000);
 
         nasaIcon.fadeIn(2000, () => loginPanel.fadeIn(3000));
-        pJSDom[0].pJS.particles.move.enable = false;
+        // pJSDom[0].pJS.particles.move.enable = false;
     });
 
 
-    requestAPI().then(getColumns())
+    //requestAPI().then(getColumns());
 
+
+
+
+    console.log(login);
+    register.on("click", showRegister);
+    login.on("click", showLogin)
+
+
+
+    function showRegister() {
+
+        register.addClass("option-selected");
+        registerForm.removeClass("hidden");
+
+        login.removeClass("option-selected");
+        loginForm.addClass("hidden");
+
+    }
+
+    function showLogin() {
+
+        console.log('im clicked bitch!');
+        login.addClass("option-selected");
+        loginForm.removeClass("hidden");
+
+        register.removeClass("option-selected");
+        registerForm.addClass("hidden");
+
+    }
 
     // post
 
@@ -34,11 +67,6 @@ window.onload = function () {
         return await res.text();
     }
     */
-
-
-    function showLogin() {
-
-    }
 
     function hideLogin() {
 
