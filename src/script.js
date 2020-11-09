241b590c54372b283436337585c3aab37a9df12a
Berni <bernatferrerm@gmail.com>
2020-11-09 12:54:05 +0100

    login validation

--- src/script.js
+++ src/script.js
@@ -15,13 +15,11 @@ window.onload = function () {
         loginTitle.fadeIn(5000);
 
         nasaIcon.fadeIn(2000, () => loginPanel.fadeIn(3000));
-        // pJSDom[0].pJS.particles.move.enable = false;
+        pJSDom[0].pJS.particles.move.enable = false;
     });
 
 
     //requestAPI().then(getColumns());
-
-
 
 
     console.log(login);
@@ -45,12 +43,16 @@ window.onload = function () {
         console.log('im clicked bitch!');
         login.addClass("option-selected");
         loginForm.removeClass("hidden");
-
         register.removeClass("option-selected");
         registerForm.addClass("hidden");
 
     }
 
+
+    $('#signin').on("click", function (event) {
+        event.preventDefault();
+        validateLogin();
+    })
     // post
 
     /*
@@ -71,11 +73,51 @@ window.onload = function () {
     function hideLogin() {
 
         pJSDom[0].pJS.particles.move.enable = false;
-
         sectionLogin.fadeOut(5000);
+    }
 
+    function validateLogin() {
+
+        var formData = Object.fromEntries(new FormData(document.querySelector('.login-form')).entries());
+        var errName = "username should contain more <br> than three characters";
+        var errPassword = "password must contain an uppercase, <br> a number, and 6 characters minimum";
+        var inputs = Array.from(loginForm.children());
+        inputs.pop();
+        var conditions = [/\b.{3,}\b/, /(?=.*\d)(?=.*[A-Z]).{6,}/];
+        var errors = [errName, errPassword];
+        console.log(inputs);
+        validateLoop(inputs, conditions, errors);
     }
 
 
+    /* validation */
+    function validate(input, condition, errorMsg) {
+        let validation = true;
+        if (!condition.test($(input).val())) {
+            $(input).after("<div class='error-msg col-lg-12 col-md-8'><p>" + errorMsg + "</p></div>");
+            $(input).addClass("error-input")
+            validation = false;
+        }
+        return validation; // true if validation passed, else false
+    }
+
+    function validateLoop(inputs, conditions, messages) {
+        clearErrors();
+        var valid = true;
+        inputs.forEach(function (input, i) {
+            if (!validate(input, conditions[i], messages[i])) valid = false;
+
+        })
+
+        return valid;
+    }
+
+    // clear form errors
+    function clearErrors() {
+        var errorMsg = $(".error-msg");
+        var errorInput = $(".error-input");
+        for (let div of errorMsg) $(div).remove();
+        for (let div of errorInput) $(div).classList.remove("error-input");
+    }
 
 }
